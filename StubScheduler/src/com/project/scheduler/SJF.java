20cabd71890ec1e42ba544e1ffe491167a6500b4
leonardo Farinha <leonardofarinhap@gmail.com>
2016-05-02 20:05:34 -0500

    bug fixes

--- StubScheduler/src/com/project/scheduler/SJF.java
+++ StubScheduler/src/com/project/scheduler/SJF.java
@@ -5,13 +5,15 @@ import java.util.List;
 public class SJF extends Strategy {
 	
 	//2 dimensional arrays
-	  private int process[][], temp[][];
+	  private int process[][], temp[][], ready[][];
 	  
-	  private int cpuTime = 0, process_id_cache, arrival_Time_cache, burst_time_cache; 
+	  private int cpuTime = 0, process_id_cache, arrival_Time_cache, burst_time_cache, wait_time_cache, turnaround_time_cache, start_time_cache, finish_time_cache; 
 
 	//int variables
-	  private int count = 0, avgWtTime=0, avgTrndTm=0;
+	  private int count = 0, totalWtTime=0, totalTrnTime=0;
 	 	 
+	  //boolean variables
+	  private boolean isFirstProcess = false;
 	  
 	  public SJF(List<Job> jobs) {
 		super(jobs);
@@ -24,8 +26,9 @@ public class SJF extends Strategy {
 	
 	public void run(List<Job> jobList, int quantum, int processCount)
 	{			
-			process = new int[processCount][3];
-			temp = new int[processCount][5];
+			process = new int[processCount][7];
+			temp = new int[processCount][7];
+			ready = new int[processCount][7];
 			
 			for(Job job:jobList)
 			{				
@@ -40,122 +43,188 @@ public class SJF extends Strategy {
 				count++;
 			}
 			
-				
-			for(int i=0; i<temp.length; i++)
-			{
-				for(int j=0; j<temp.length-1; j++)
-				{
-					if(temp[j+1][2]<temp[j][2])
-					{
-						this.process_id_cache = temp[j][0];
-						this.arrival_Time_cache = temp[j][1];
-						this.burst_time_cache = temp[j][2];
-						temp[j][0] = temp[j+1][0];
-						temp[j][1] = temp[j+1][1];
-						temp[j][2] = temp[j+1][2];
-						temp[j+1][0] = this.process_id_cache;
-						temp[j+1][1] = this.arrival_Time_cache;
-						temp[j+1][2] = this.burst_time_cache;
-					}
-				}
-			}
+			this.reverseProcess(temp);
 			
-			for(int i=0; i<temp.length; i++)
+			for(int i=0;i<temp.length;i++)
 			{
-				System.out.println("Process: "+temp[i][0]);
-				System.out.println("=====================================");
-				
-				if(i == 0)
+				for(int j=0;j<temp.length;j++)
 				{
-					if(temp[i][2]==temp[i+1][2])
+
+					if(temp[j][1]<cpuTime && temp[j][2]>0)
 					{
-						if(temp[i][1]<temp[i+1][1])
-						{
-							this.executeProcess(temp, i);		
-						}else if(temp[i][1]>temp[i+1][1])
-						{
-							this.executeNextProcess(temp, i);	
-						}else if(temp[i][1]==temp[i+1][1])
-						{
-							this.executeProcess(temp, i);
-						}
-					}else
+						ready[j][0] = temp[j][0];
+						ready[j][1] =temp[j][1];
+						ready[j][2] = temp[j][2];
+						cpuTime+=temp[j][2];
+						temp[j][2]=0;
+						System.out.println("< | Excecute process "+ready[j][0]+" | "+ready[j][1]+"  |  "+ready[j][2]+" cputime "+cpuTime);
+				  }else if(temp[j][1]==cpuTime && temp[j][2]>0)
 					{
-						this.executeProcess(temp, i);
-					}
-				}else if(i == temp.length-1)
-				{
-					this.executeProcess(temp, i);	
-				}else if(i < temp.length)
-				{
-					if(temp[i][2]==temp[i+1][2])
-					{
-						if(temp[i][1]<temp[i+1][1])
-						{
-							this.executeProcess(temp, i);	
-						}else if(temp[i][1]>temp[i+1][1])
-						{
-							this.executeNextProcess(temp, i);
-						}else if(temp[i][1]==temp[i+1][1])
-						{
-							this.executeProcess(temp, i);
-						}
-					}else
-					{
-						this.executeProcess(temp, i);
-					}
+						ready[j][0] = temp[j][0];
+						ready[j][1] =temp[j][1];
+						ready[j][2] = temp[j][2];
+						cpuTime+=temp[j][2];
+						temp[j][2]=0;
+						System.out.println("AT == CPUTIME | Excecute process "+ready[j][0]+" | "+ready[j][1]+"  |  "+ready[j][2]+" cputime "+cpuTime);
+				  }
 				}
+				
 			}
-	}// EOMethod
+						
+		}// EOMethod
 	
 	public void executeProcess(int[][] temp, int i)
 	{
-				temp[i][3] = cpuTime; //start time of the process
-				System.out.println("Start time "+temp[i][3]);
+			if(this.isFirstProcess == true)
+			{
+				temp[i][3] = temp[i][1]; //start time of the process
+//				System.out.println("Start time "+temp[i][3]);
+				
+				cpuTime += temp[i][2]+temp[i][3]; // cpu time
+				temp[i][4] = cpuTime;
+//				System.out.println("cpu time consumed: "+cpuTime);
+
+				
+//				System.out.println("Process: "+temp[i][0]+" Finished");
+//				System.out.println("=====================================");
+				this.isFirstProcess = false;
+			}else
+			{
+				temp[i][3] = temp[i-1][4]; //start time of the process
+//				System.out.println("Start time "+temp[i][3]);
 				
 				cpuTime += temp[i][2]; // cpu time
-				System.out.println("cpu time consumed: "+cpuTime);
+				temp[i][4] = cpuTime; // FINISH TIME
+//				System.out.println("cpu time consumed: "+cpuTime);
 				
-				temp[i][4] =  temp[i][3] + temp[i][2];
-				System.out.println("Finish time: "+temp[i][4]);
 				
-				temp[i][2] = 0;
-				System.out.println("Process: "+temp[i][0]+" Finished");
-				System.out.println("=====================================");	
+//				System.out.println("Process: "+temp[i][0]+" Finished");
+//				System.out.println("=====================================");
+			}
 	}
 	
 	public void executeNextProcess(int[][] temp, int i)
 	{
-				temp[i+1][3] = cpuTime; //start time of the process
-				System.out.println("Start time "+temp[i+1][3]);
+				temp[i+1][3] = temp[i-1][4]; //start time of the process
+//				System.out.println("Start time "+temp[i+1][3]);
 				
 				cpuTime += temp[i+1][2]; // cpu time
-				System.out.println("cpu time consumed: "+cpuTime);
-				
-				temp[i+1][4] =  temp[i+1][3] + temp[i+1][2];
-				System.out.println("Finish time: "+temp[i+1][4]);
-				
-				temp[i+1][2] = 0;
-				System.out.println("Process: "+temp[i+1][0]+" Finished");
-				System.out.println("=====================================");	
+				temp[i][4] = cpuTime;
+
+				//				System.out.println("Process: "+temp[i+1][0]+" Finished");
+//				System.out.println("=====================================");
+	}
+		
+	public void reverseProcess(int[][] temp)
+	{
+		for(int i=0; i<temp.length; i++)
+		{
+			for(int j=0; j<temp.length-1; j++)
+			{
+				if(temp[j+1][2]<temp[j][2])
+				{
+					this.process_id_cache = temp[j][0];
+					this.arrival_Time_cache = temp[j][1];
+					this.burst_time_cache = temp[j][2];
+					temp[j][0] = temp[j+1][0];
+					temp[j][1] = temp[j+1][1];
+					temp[j][2] = temp[j+1][2];
+					temp[j+1][0] = this.process_id_cache;
+					temp[j+1][1] = this.arrival_Time_cache;
+					temp[j+1][2] = this.burst_time_cache;
+				}
+			}
+		}
+	}
+	
+	public void arrangeProcess(int[][] temp)
+	{
+		for(int i=0; i<temp.length; i++)
+		{
+			for(int j=0; j<temp.length-1; j++)
+			{
+				if(temp[j+1][0]<temp[j][0])
+				{
+					this.process_id_cache = temp[j+1][0];
+					this.arrival_Time_cache = temp[j+1][1];
+					this.burst_time_cache = temp[j+1][2];
+					temp[j+1][0] = temp[j][0];
+					temp[j+1][1] = temp[j][1];
+					temp[j+1][2] = temp[j][2];
+					temp[j][0] = this.process_id_cache;
+					temp[j][1] = this.arrival_Time_cache;
+					temp[j][2] = this.burst_time_cache;
+				}
+			}
+		}
+		for(int i=0; i<temp.length; i++)
+		{
+			for(int j=0; j<temp.length-1; j++)
+			{
+				if(temp[j+1][0]<temp[j][0])
+				{
+					this.process_id_cache = temp[j+1][0];
+					this.arrival_Time_cache = temp[j+1][1];
+					this.burst_time_cache = temp[j+1][2];
+					this.start_time_cache = temp[j+1][3];
+					this.finish_time_cache = temp[j+1][4];
+					this.turnaround_time_cache = temp[j+1][5];
+					this.wait_time_cache = temp[j+1][6];
+					temp[j+1][0] = temp[j][0];
+					temp[j+1][1] = temp[j][1];
+					temp[j+1][2] = temp[j][2];
+					temp[j+1][3] = temp[j][3];
+					temp[j+1][4] = temp[j][4];
+					temp[j+1][5] = temp[j][5];
+					temp[j+1][6] = temp[j][6];
+					temp[j][0] = this.process_id_cache;
+					temp[j][1] = this.arrival_Time_cache;
+					temp[j][2] = this.burst_time_cache;
+					temp[j][3] = this.start_time_cache;
+					temp[j][4] = this.finish_time_cache;
+					temp[j][5] = this.turnaround_time_cache;
+					temp[j][6] = this.wait_time_cache;
+				}
+				}
+			System.out.println(temp[i][0]+" | "+temp[i][2]+" | "+temp[i][3]+" | "+temp[i][4]+" | "+temp[i][5]+" | "+temp[i][6]);
+			}
 	}
 	
 	public void print()
 	{
+		this.arrangeProcess(temp);
+		
+		/*
+		 * temp[*][0] == PROCESS ID
+		 * temp[*][1] == ARRIVAL TIME
+		 * temp[*][2] == BURST TIME
+		 * temp[*][3] == START TIME
+		 * temp[*][4] == FINISH TIME
+		 * temp[*][5] == TURNAROUND TIME
+		 * temp[*][6] == WAIT TIME
+		*/
 		for(int i=0; i<process.length; i++)
 		{
-			this.avgWtTime += temp[i][3] - temp[i][1];
-			System.out.println("Avg Wt Time"+this.avgWtTime);
-			this.avgTrndTm += temp[i][2] + this.avgWtTime;
-			System.out.println("Avg Trn Time"+this.avgTrndTm);
-						
+			temp[i][6] =  temp[i][3] - temp[i][1];
+			this.totalWtTime += temp[i][6];
+			temp[i][5] = process[i][2] + temp[i][6];
+			this.totalTrnTime += temp[i][5];
+
 		}
+				
+		double throughput = (this.process.length/this.cpuTime)*1000;
+				
+		System.out.println("\nTotal Run Time: "+this.cpuTime);
+		System.out.println("Average Waiting Time: " +(this.totalWtTime/process.length));
+		System.out.println("Average Turnaround Time: " + (this.totalTrnTime/process.length));
+		System.out.println("Throughput: " + throughput+" sec\n");
 		
-//		System.out.println("=============================================");
-//		System.out.println("Total Run Time: "+this.cpuTime);
-//		System.out.println("Average Waiting Time: " +(this.avgWtTime/process.length));
-//		System.out.println("Average Turnaround Time: " + (this.avgTrndTm/process.length));
-//		System.out.println("=============================================");
+		System.out.println("|PID||Arrival Time||Start Time||Finish Time||Wait Time||Turnaround Time|");
+		for(int i=0; i<process.length; i++)
+		{
+		System.out.println("  "+this.temp[i][0]+" \t   "+this.temp[i][1]+"\t        "+this.temp[i][3]+"\t     "+this.temp[i][4]+"\t        "+this.temp[i][6]+"\t      "+this.temp[i][5]);
+		System.out.println("------------------------------------------------------------------------------------------");
+		}
 		
 		
 	}
